a3ee82d97b8482821b3be3fbef656131d708fab9
Akib-12 <37499896+Akib-12@users.noreply.github.com>
2020-07-31 10:47:22 +0600

    changed to functional component

--- src/components/campaign_form/campaign_form.jsx
+++ src/components/campaign_form/campaign_form.jsx
@@ -1,115 +1,114 @@
-import React, {Component} from 'react';
-import {addCampaign} from "../../store/campaignFormSlice";
+import React, {useEffect, useState} from 'react';
 import M from 'materialize-css'
-import {connect} from "react-redux";
+import {useDispatch} from "react-redux";
 import {v4 as uuidV4} from 'uuid';
 import ProjectModel from "../../dataModels/ProjectModel";
 import {projectTypes} from "../../dataModels/ProjectTypes";
+import produce from 'immer'
+import {addCampaign} from "../../store/campaignFormSlice";
 
-class CampaignForm extends Component {
-    state = ProjectModel()
+const CampaignForm = () => {
+    const [project, setProject] = useState(ProjectModel())
+    const dispatch = useDispatch()
 
-    componentDidMount() {
+    useEffect(() => {
         M.FormSelect.init(document.getElementById('project_type'))
-    }
+    }, [])
 
-    render() {
-        return (
-            <div>
-                {/*<div className="navbar-fixed">*/}
-                {/*    <nav style={{height: "64px"}} className="white z-depth-2">*/}
-                {/*        <div className="nav-wrapper">*/}
-                {/*            <ul className="left">*/}
-                {/*                <li><LinkBrand to="/"><img style={{height: "60px"}} src={logo}*/}
-                {/*                                           alt="INNOFUND"/></LinkBrand></li>*/}
-                {/*            </ul>*/}
-                {/*            <ul className="right">*/}
-                {/*                <li><LinkStyled to="/about">About Us</LinkStyled></li>*/}
-                {/*            </ul>*/}
-                {/*        </div>*/}
-                {/*    </nav>*/}
-                {/*</div>*/}
-                {/*NavBar*/}
-                <div className="container">
-                    <h3>Home Start A Project</h3>
-                    <form onSubmit={this.handleSubmit}>
-                        <div>
-                            <input type='text' onChange={this.handleChange} value={this.state.project_name}
-                                   name={'project_name'} placeholder={'project_name'}/>
-                            <input type='text' onChange={this.handleChange} value={this.state.project_type}
-                                   name={'project_type'} placeholder={'project_type'}/>
-                            <textarea className='materialize-textarea' onChange={this.handleChange}
-                                      value={this.state.project_description}
-                                      name={'project_description'} placeholder={'project_description'}/>
-                            <input type='text' onChange={this.handleChange} value={this.state.start_date}
-                                   name={'start_date'}
-                                   placeholder={'start_date'}/>
-                            <input type='text' style={{marginBottom: 50}} onChange={this.handleChange}
-                                   value={this.state.end_date}
-                                   name={'end_date'}
-                                   placeholder={'end_date'}/>
-                            <label className='row s2 teal-text darken-4' style={{fontWeight: "bold", fontSize: 15}}>Goal
-                                Money
-                                <input className='row s10' style={{marginBottom: 50}} type='number'
-                                       onChange={this.handleChange}
-                                       value={this.state.goal_money}
-                                       name={'goal_money'}
-                                       placeholder='goal_money'/>
-                            </label>
-
-                            <label className='row s2 teal-text darken-4'
-                                   style={{fontWeight: "bold", fontSize: 15}}>Select
-                                Project Type
-                            </label>
-
-
-                            <select id='project_type'>
-                                <option value={projectTypes.EQUITY_BASED}>{projectTypes.EQUITY_BASED}</option>
-                                <option value={projectTypes.REWARD_BASED}>{projectTypes.REWARD_BASED}</option>
-                                <option value={projectTypes.PROFIT_SHARING}>{projectTypes.PROFIT_SHARING}</option>
-                            </select>
-
-
-                            <input type='text' style={{marginTop: 20}} onChange={this.handleChange}
-                                   value={this.state.project_showcasing_video_url}
-                                   name={'project_showcasing_video_url'}
-                                   placeholder={'Your Project Video Url'}/>
-
-
-                        </div>
-
-                        <button style={{
-                            width: "150px",
-                            borderRadius: "3px",
-                            letterSpacing: "1.5px",
-                            marginTop: "3rem"
-                        }}
-                                type="submit"
-                                className="btn btn-large waves-effect waves-light hoverable indigo darken-1">Next
-                        </button>
-                    </form>
-                </div>
-            </div>
-        );
-    }
-
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.addProject({...this.state, id: uuidV4()})
+        console.log(project)
+        dispatch(addCampaign({...project, id: uuidV4()}))
         //TODO: Routing kore homePage e jabe if the credentials are correct
     }
 
-    handleChange = (event) => {
+    const handleChange = (event) => {
         const {name, value} = event.target;
-        this.setState({
-            [name]: value
-        })
+        setProject(produce(project, draft => {
+            draft[name] = value
+        }))
         // console.log(this.state)
     }
+
+    return (
+        <div>
+            {/*<div className="navbar-fixed">*/}
+            {/*    <nav style={{height: "64px"}} className="white z-depth-2">*/}
+            {/*        <div className="nav-wrapper">*/}
+            {/*            <ul className="left">*/}
+            {/*                <li><LinkBrand to="/"><img style={{height: "60px"}} src={logo}*/}
+            {/*                                           alt="INNOFUND"/></LinkBrand></li>*/}
+            {/*            </ul>*/}
+            {/*            <ul className="right">*/}
+            {/*                <li><LinkStyled to="/about">About Us</LinkStyled></li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
+            {/*    </nav>*/}
+            {/*</div>*/}
+            {/*NavBar*/}
+            <div className="container">
+                <h3>Home Start A Project</h3>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input type='text' onChange={handleChange} value={project.project_name}
+                               name={'project_name'} placeholder={'project_name'}/>
+                        <input type='text' onChange={handleChange} value={project.project_type}
+                               name={'project_type'} placeholder={'project_type'}/>
+                        <textarea className='materialize-textarea' onChange={handleChange}
+                                  value={project.project_description}
+                                  name={'project_description'} placeholder={'project_description'}/>
+                        <input type='text' onChange={handleChange} value={project.start_date}
+                               name={'start_date'}
+                               placeholder={'start_date'}/>
+                        <input type='text' style={{marginBottom: 50}} onChange={handleChange}
+                               value={project.end_date}
+                               name={'end_date'}
+                               placeholder={'end_date'}/>
+                        <label className='row s2 teal-text darken-4' style={{fontWeight: "bold", fontSize: 15}}>Goal
+                            Money
+                            <input className='row s10' style={{marginBottom: 50}} type='number'
+                                   onChange={handleChange}
+                                   value={project.goal_money}
+                                   name={'goal_money'}
+                                   placeholder='goal_money'/>
+                        </label>
+
+                        <label className='row s2 teal-text darken-4'
+                               style={{fontWeight: "bold", fontSize: 15}}>Select
+                            Project Type
+                        </label>
+
+
+                        <select id='project_type'>
+                            <option value={projectTypes.EQUITY_BASED}>{projectTypes.EQUITY_BASED}</option>
+                            <option value={projectTypes.REWARD_BASED}>{projectTypes.REWARD_BASED}</option>
+                            <option value={projectTypes.PROFIT_SHARING}>{projectTypes.PROFIT_SHARING}</option>
+                        </select>
+
+
+                        <input type='text' style={{marginTop: 20}} onChange={handleChange}
+                               value={project.project_showcasing_video_url}
+                               name={'project_showcasing_video_url'}
+                               placeholder={'Your Project Video Url'}/>
+
+
+                    </div>
+
+                    <button style={{
+                        width: "150px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "3rem"
+                    }}
+                            type="submit"
+                            className="btn btn-large waves-effect waves-light hoverable indigo darken-1">Next
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+
+
 }
 
-const mapDispatchToProps = dispatch => ({
-    addProject: (project) => dispatch(addCampaign(project))
-})
-
-export default connect(() => ({}), mapDispatchToProps)(CampaignForm);
+export default CampaignForm
