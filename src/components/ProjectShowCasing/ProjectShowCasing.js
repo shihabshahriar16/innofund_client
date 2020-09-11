cfba558a8c0574b61ef711897cd171052c59b087
Amimul Ehsan <50029846+nkshn22@users.noreply.github.com>
2020-09-11 08:28:05 +0600

    Added Card
    Added Card for Project Showcasing

--- src/components/ProjectShowCasing/ProjectShowCasing.js
+++ src/components/ProjectShowCasing/ProjectShowCasing.js
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {deleteCampaign} from "../../store/campaignFormSlice";
 import {Link} from "react-router-dom";
 import router from "../../routing/routing_variables";
+import Card from "../../Cards/CardUI";
+import img1 from "../../images/logo.png";
 
 const ProjectShowCasing = () => {
     const projects = useSelector(state => state.projectsInStore)
@@ -12,21 +14,29 @@ const ProjectShowCasing = () => {
     }
 
     return (
-        <div>
-            {projects.map(project => {
-                return <div key={project.id}>
-                    <div>
-                        {project.project_name} {project.project_description} {project.goal_money}
-                    </div>
-                    <button className='btn-small' onClick={() => handleDelete(project.id)}>Delete</button>
-                    <Link to={{
-                        pathname: router.PROJECT_DETAILS,
-                        state: project
-                    }}>
-                        <button className='btn-large'>Details</button>
-                    </Link>
-                </div>
+        <div className="container-fluid d-flex justify-content-center">
+            <div className="row">
+                {projects.map(project => {
+                    /*return <div key={project.id}>
+                        <div>
+                            {project.project_name} {project.project_description} {project.goal_money}
+                        </div>
+                        <button className='btn-small' onClick={() => handleDelete(project.id)}>Delete</button>
+                        <Link to={{
+                            pathname: router.PROJECT_DETAILS,
+                            state: project
+                        }}>
+                            <button className='btn-large'>Details</button>
+                        </Link>
+                    </div>*/
+
+                    return(
+                        <div className="col-md-6">
+                            <Card project ={project} imgsrc={img1} title={project.project_name} body={project.project_description}/>
+                        </div>
+                    )
             })}
+            </div>
         </div>
     );
 };
