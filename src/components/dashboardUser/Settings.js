67d68e740b8f84d058cc805ab97b36009e98a639
refaat31 <refaatalamgir@iut-dhaka.edu>
2020-09-17 23:34:43 +0600

    Get logged in user's profile (simple)

--- src/components/dashboardUser/Settings.js
+++ src/components/dashboardUser/Settings.js
@@ -1,13 +1,73 @@
-import React, { Component } from 'react';
+import { immerable } from 'immer';
+import React, { useState, useEffect, Fragment } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-class Settings extends Component {
-    render() {
-        return (
-            <div>
-                <h5>My settings</h5>
-            </div>
-        )
-    }
-}
+import produce from 'immer';
+import { loadMyProfile_Action } from '../../store/profileSlice';
+const Settings = () => {
+  const dispatch = useDispatch();
+  const myProfile = useSelector((state) => state.myProfile);
+  useEffect(() => {
+    console.log('get user profile here ');
+    dispatch(loadMyProfile_Action());
+  }, [dispatch]);
+
+  return (
+    <Fragment>
+      <div className='row'>
+        <div className='input-field col s6'>
+          <input
+            value={myProfile.name}
+            id='name'
+            type='text'
+            className='validate'
+          />
+          <br/>
+          <label class='active' for='name'>
+            Name
+          </label>
+        </div>
+        <div className='input-field col s6'>
+          <input
+            value={myProfile.email}
+            id='email'
+            type='text'
+            className='validate'
+          />
+          <br/>
+          <label class='active' for='email'>
+            Email
+          </label>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='input-field col s6'>
+          <input
+            value={myProfile.organization}
+            id='organization'
+            type='text'
+            className='validate'
+          />
+          <br/>
+          <label class='active' for='organization'>
+            Organization
+          </label>
+        </div>
+        <div className='input-field col s6'>
+          <input
+            value={myProfile.position}
+            id='email'
+            type='text'
+            className='validate'
+          />
+          <br/>
+          <label class='active' for='position'>
+            Position
+          </label>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default Settings;
---
+++ src/store/profileSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import qs from 'querystring';
+
+const store = createSlice({
+  name: 'myProfile',
+  initialState: {
+    name: '',
+    email: '',
+    organization: '',
+    position: '',
+  },
+  reducers: {
+    loadMyProfile: (myProfile, action) => {
+      const { name, email, organization, position } = action.payload;
+      myProfile.name = name;
+      myProfile.email = email;
+      myProfile.organization = organization;
+      myProfile.position = position;
+    },
+  },
+});
+
+export const loadMyProfile_Action = () => (dispatch) => {
+  axios
+    .get('/api/profile/me')
+    .then((res) => dispatch(loadMyProfile(res.data)))
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const { loadMyProfile } = store.actions;
+export default store.reducer;
